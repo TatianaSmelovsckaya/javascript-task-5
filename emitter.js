986e20258170752716ace209ed7b651deb2a9b40
Смеловская Татьяна <tatiana.smelovsckaya@ya.ru>
2017-10-28 21:02:03 +0500

    Пора на лекцию

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
 
 /**
@@ -13,32 +13,69 @@ module.exports = getEmitter;
  */
 function getEmitter() {
     return {
+        subscriptions: [],
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            this.subscriptions.push({
+                event: event,
+                context: context,
+                handler: handler
+            });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            this.subscriptions.forEach(function (subscription, i) {
+                let subscriptionEvent = subscription.event;
+                let eventIndex = subscriptionEvent.indexOf(event);
+                let nextSymbol = subscriptionEvent[eventIndex + event.length];
+
+                if (context === subscription.context && eventIndex === 0 &&
+                    (nextSymbol === '.' || nextSymbol === undefined)) {
+                    delete this.subscriptions[i];
+                }
+            }, this);
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            let eventParts = event.split('.');
+            let events = [];
+
+            for (let i = 0; i < eventParts.length; i++) {
+                let newEvent = eventParts.slice(0, i + 1).join('.');
+                events.unshift(newEvent);
+            }
+
+            events.forEach(function (e) {
+                this.subscriptions.forEach(function (subscription) {
+                    if (subscription.event === e) {
+                        subscription.handler.call(subscription.context);
+                    }
+                });
+            }, this);
+
+            return this;
         },
 
         /**
